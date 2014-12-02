335ec65050e6e76f78faedfcd8d4fc8c55f7f7d2
alira <souilleux.amandine@gmail.com>
2014-12-02 17:53:28 +0100

    création de la structure des voeux

---
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/WishState.java
@@ -0,0 +1,7 @@
+package org.jboss.as.quickstarts.kitchensink.model;
+
+public enum WishState {
+	
+	NOT_VALIDATED, DENIED, ACCEPTED
+
+}
--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/Wishes.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/Wishes.java
@@ -1,5 +1,41 @@
 package org.jboss.as.quickstarts.kitchensink.model;
 
-public class Wishes {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@SuppressWarnings("serial")
+@Entity
+@XmlRootElement
+public class Wishes implements Serializable {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	@NotNull
+	@NotEmpty
+	@Email
+	private String memberId;
+	
+	@NotNull
+	@NotEmpty
+	@Enumerated(EnumType.STRING)
+	private WishState etat;
+	
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp="[0-9]*")
+	private long idCours;
 
 }
