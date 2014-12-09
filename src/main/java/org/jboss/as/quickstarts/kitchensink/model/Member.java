b4a4c2c5348aa96288f814c1ba9af847f0434a24
Teo <teogeos@hotmail.com>
2014-12-09 13:55:34 +0100

    test teo bureau 1/2

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/Member.java
@@ -32,7 +32,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
-
+//letest
 @SuppressWarnings("serial")
 @Entity
 @XmlRootElement
