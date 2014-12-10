ce0e2790cc0c478a19e0befc8ccd4d3e110e8c9b
amandineslx <souilleux.amandine@gmail.com>
2014-12-10 17:01:02 +0100

    correction relation entre Intervention et FormePedago

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/FormePedago.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/FormePedago.java
@@ -1,7 +1,10 @@
 package org.jboss.as.quickstarts.kitchensink.model;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -24,6 +27,9 @@ public class FormePedago {
 	@NotEmpty
 	private String formule;
 	
+	@OneToMany
+	private Collection<Intervention> interventions;
+	
 	public String getFormePedago() {
 		return this.formePedago;
 	}
@@ -38,6 +44,10 @@ public class FormePedago {
 	
 	public void setFormule(String formule) {
 		this.formule=formule;
+	}
+	
+	public Collection<Intervention> getInterventions() {
+		return this.interventions;
 	}
 
 }
