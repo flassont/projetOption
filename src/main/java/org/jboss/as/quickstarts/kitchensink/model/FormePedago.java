bf327adfa4ab1332645c3f95d1acb14e25d33537
amandineslx <souilleux.amandine@gmail.com>
2014-12-10 14:56:06 +0100

    vérification et maj FormePedago

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/FormePedago.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/model/FormePedago.java
@@ -3,6 +3,7 @@ package org.jboss.as.quickstarts.kitchensink.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -14,6 +15,29 @@ public class FormePedago {
 	@Id
 	private String formePedago;
 	
+	@NotNull
+	@NotEmpty
+	@Pattern(regexp="[2-9][0-9][0-9][0-9]")
+	private int annee;
 	
+	@NotNull
+	@NotEmpty
+	private String formule;
+	
+	public String getFormePedago() {
+		return this.formePedago;
+	}
+	
+	public int getAnnee() {
+		return this.annee;
+	}
+	
+	public String getFormule() {
+		return this.formule;
+	}
+	
+	public void setFormule(String formule) {
+		this.formule=formule;
+	}
 
 }
