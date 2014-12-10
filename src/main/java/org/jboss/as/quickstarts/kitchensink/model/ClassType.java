b317d62fa2084eb729e0306f320368e93f0e8dbc
Nicolas Humeau <nicolas.humeau@gmail.com>
2014-12-10 13:52:56 +0100

    Bug Git

--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/ClassType.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.as.quickstarts.kitchensink.model;
-
-public enum ClassType {
-	
-	CM, TP, TD, PC
-
-}
--- src/main/java/org/jboss/as/quickstarts/kitchensink/model/RelationState.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.as.quickstarts.kitchensink.model;
-
-public enum RelationState {
-	
-	NOT_YET_VALIDATED, DENIED, ACCEPTED
-
-}
