93d3549d030b707bfad638ae5ad85465cb0f2c85
Teo <teogeos@hotmail.com>
2014-12-15 01:17:21 +0100

    Implementation de la vue de l'ajout de module à un UV

--- src/main/java/org/jboss/as/quickstarts/kitchensink/rest/ResponsabiliteResourceRESTService.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/rest/ResponsabiliteResourceRESTService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
