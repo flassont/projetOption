b38b9bc7bb89fbdd6753b0ac4863b30ca1f73b8d
Teo <teogeos@hotmail.com>
2015-01-14 09:25:02 +0100

    Creation de RelationPredecessor
    Sette classe enveloppe les donnees necessaires pour creer une Relation, donnees qui seront envoyees par le front end.

---
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/util/RelationPredecessor.java
@@ -0,0 +1,10 @@
+package org.jboss.as.quickstarts.kitchensink.util;
+
+/**
+ * Created by Téo on 14/01/2015.
+ */
+public class RelationPredecessor {
+    private String emailIntervenant;
+    private long idResponsabilite;
+    private String eunm;
+}
