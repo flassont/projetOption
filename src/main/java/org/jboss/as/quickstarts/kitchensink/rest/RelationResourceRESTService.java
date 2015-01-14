1a0e914b4ad05a508dd6e6f3584448f8fc7bad12
Teo <teogeos@hotmail.com>
2015-01-14 09:20:56 +0100

    Creation de RelationResourceRESTService
    Cette classe servira a implementer les services de creation/modification de relations

---
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/rest/RelationResourceRESTService.java
@@ -0,0 +1,13 @@
+package org.jboss.as.quickstarts.kitchensink.rest;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ * Created by Téo on 14/01/2015.
+ */
+@Path("/relations")
+@RequestScoped
+public class RelationResourceRESTService {
+
+}
