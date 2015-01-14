cf62762fc8837902afad7d3dddacb50e50637614
Teo <teogeos@hotmail.com>
2015-01-14 10:20:41 +0100

    Avancement implementation REST back end Relation

--- src/main/java/org/jboss/as/quickstarts/kitchensink/rest/RelationResourceRESTService.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/rest/RelationResourceRESTService.java
@@ -1,7 +1,15 @@
 package org.jboss.as.quickstarts.kitchensink.rest;
 
+import org.jboss.as.quickstarts.kitchensink.model.UV;
+import org.jboss.as.quickstarts.kitchensink.util.RelationPredecessor;
+
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by Téo on 14/01/2015.
@@ -10,4 +18,11 @@ import javax.ws.rs.Path;
 @RequestScoped
 public class RelationResourceRESTService {
 
+    @POST
+    @Path("/uvs")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createRelation (RelationPredecessor relationPredecessor) {
+        return null;
+    }
 }
