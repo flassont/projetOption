4f9eef40a59fdba31484b9dff6d88ddc6b8d490b
teogeos <teogeos@hotmail.com>
2015-02-25 17:10:52 +0100

    Nettoyage code 1

--- src/main/java/org/jboss/as/quickstarts/kitchensink/service/MemberRegistration.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/service/MemberRegistration.java
@@ -1,19 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2013, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jboss.as.quickstarts.kitchensink.service;
 
 import org.jboss.as.quickstarts.kitchensink.model.Intervenant;
@@ -41,19 +25,16 @@ public class MemberRegistration {
     public void update(Intervenant member) throws Exception {
         log.info("Updating " + member.getName());
         em.merge(member);
-        memberEventSrc.fire(member);
     }
     
     public void register(Intervenant member) throws Exception {
         log.info("Registering " + member.getName());
         em.persist(member);
-        memberEventSrc.fire(member);
     }
     
     public void delete(String email) throws Exception {
         Intervenant member = em.find(Intervenant.class,email);
         log.info("Deleting " + member.getName());
         em.remove(member);
-        memberEventSrc.fire(member);
     }
 }
